82a7106e3799166ba3aa7f5b8fad10201b3a3b4f
abhishek9125 <aagarwal_be17@thapar.edu>
2022-09-19 21:05:24 +0530

    Doubly Linked List

---
+++ Linked List/2. Doubly Linked List.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+
+    public:
+        int data;
+        Node* prev;
+        Node* next;
+
+        Node(int data) {
+            this->data = data;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+    
+        ~Node() {
+            if(this->next != NULL) {
+                this->next = NULL;
+                delete next;
+            }
+
+            if(this->prev != NULL) {
+                this->prev = NULL;
+                delete prev;
+            }
+        }
+};
+
+void insertAtHead(Node*& head, int data) {
+
+    Node* temp = new Node(data);
+
+    if(head == NULL) {
+        head = temp;
+        return;
+    }
+
+    temp->next = head;
+    head->prev = temp;
+    head = temp;
+
+    return;
+}
+
+void insertAtTail(Node*& head, int data) {
+
+    Node* temp = new Node(data);
+
+    if(head == NULL) {
+        head = temp;
+        return;
+    }
+
+    Node* tail = head;
+
+    while(tail->next != NULL) {
+        tail = tail->next;
+    }
+
+    temp->prev = tail;
+    tail->next = temp;
+
+    return;
+}
+
+void insertAtPosition(Node*& head, int position, int data) {
+
+    Node* temp = new Node(data);
+
+    if(head == NULL) {
+        head = temp;
+        return;
+    }
+
+    if(position == 1) {
+        insertAtHead(head, data);
+        return;
+    }
+
+    int count = 1;
+    Node* track = head;
+
+    while(count < position - 1) {
+        track = track->next;
+        count++;
+    }
+
+    if(track->next == NULL) {
+        insertAtTail(head, data);
+    }
+
+    track->next->prev = temp;
+    temp->next = track->next;
+    temp->prev = track;
+    track->next = temp;
+
+    return;
+}
+
+void deleteNode(Node*& head, int position) {
+
+    
+
+    return;
+}
+
+void printList(Node*& head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << endl;
+    return;
+}
+
+int getLen(Node* head) {
+
+    int length = 0;
+    Node* temp = head;
+
+    while(temp != NULL) {
+        length++;
+        temp = temp->next;
+    }
+
+    return length;
+}
+
+int main() {
+
+    Node* node1 = new Node(10);
+
+    return 0;
+}
